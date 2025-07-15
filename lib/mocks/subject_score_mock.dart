60d3bc0f3ec956886caff077d84621cf90fbabba
Lehm-win <kira.free.masato@gmail.com>
2025-07-16 00:11:31 +0900

    fix:mockの修正

--- lib/mocks/subject_score_mock.dart
+++ lib/mocks/subject_score_mock.dart
@@ -6,34 +6,11 @@ final List<Map<String, dynamic>> mockSubjectScores = [
   {'label': '社会', 'value': 42},
 ];
 
+final subjects = ['国語', '数学', '英語', '理科', '社会'];
+final groups = ['20', '40', '60', '80', '100'];
+
 final List<Map<String, dynamic>> mockSubjectGridScores = [
-  {'label': '国語', 'value': 20.0, 'group': '20'},
-  {'label': '数学', 'value': 20.0, 'group': '20'},
-  {'label': '英語', 'value': 20.0, 'group': '20'},
-  {'label': '理科', 'value': 20.0, 'group': '20'},
-  {'label': '社会', 'value': 20.0, 'group': '20'},
-
-  {'label': '国語', 'value': 40.0, 'group': '40'},
-  {'label': '数学', 'value': 40.0, 'group': '40'},
-  {'label': '英語', 'value': 40.0, 'group': '40'},
-  {'label': '理科', 'value': 40.0, 'group': '40'},
-  {'label': '社会', 'value': 40.0, 'group': '40'},
-
-  {'label': '国語', 'value': 60.0, 'group': '60'},
-  {'label': '数学', 'value': 60.0, 'group': '60'},
-  {'label': '英語', 'value': 60.0, 'group': '60'},
-  {'label': '理科', 'value': 60.0, 'group': '60'},
-  {'label': '社会', 'value': 60.0, 'group': '60'},
-
-  {'label': '国語', 'value': 80.0, 'group': '80'},
-  {'label': '数学', 'value': 80.0, 'group': '80'},
-  {'label': '英語', 'value': 80.0, 'group': '80'},
-  {'label': '理科', 'value': 80.0, 'group': '80'},
-  {'label': '社会', 'value': 80.0, 'group': '80'},
-
-  {'label': '国語', 'value': 100.0, 'group': '100'},
-  {'label': '数学', 'value': 100.0, 'group': '100'},
-  {'label': '英語', 'value': 100.0, 'group': '100'},
-  {'label': '理科', 'value': 100.0, 'group': '100'},
-  {'label': '社会', 'value': 100.0, 'group': '100'},
+  for (var group in groups)
+    for (var subject in subjects)
+      {'label': subject, 'value': double.parse(group), 'group': group},
 ];
